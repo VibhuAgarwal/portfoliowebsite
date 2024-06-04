e58ac6cea8abd623ddf45da023978ffba60a8101
Vibhor Agarwal <agarwalvibhor930@gmail.com>
2024-06-04 22:16:03 +0530

    Completed experience section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
+import Experience from "@/components/Experience";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { navItems } from "@/data";
-import Image from "next/image";
-import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
@@ -18,6 +17,7 @@ export default function Home() {
         <Hero />
         <Grid />
         <RecentProjects />
+        <Experience />
       </div>
     </main>
   );
